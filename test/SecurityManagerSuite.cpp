839f9cd96ca2dd245dc10b1335c4ff8bc054d1c8
rceccatelli <rebecca.ceccatelli@stud.unifi.it>
2020-12-05 12:39:42 +0100

    Added unit-testing code for SecurityManager::checkCurrentPin() and SecurityManager::setNewPin().

--- test/SecurityManagerSuite.cpp
+++ test/SecurityManagerSuite.cpp
@@ -5,19 +5,24 @@
 #include "gtest/gtest.h"
 
 #include "../project_files/profile_classes/SecurityManager.h"
+#include "../project_files/login_classes/Account.h"
 
 class SecurityManagerSuite : public testing::Test {
 
 protected:
     virtual void SetUp(){
-        settedSecurityManager = new SecurityManager("Quanti giorni ha novembre?", "30", false, nullptr);
+        account = new Account("1234567","12345","Carlo Servetti");
+        settedSecurityManager = new SecurityManager("Quanti giorni ha novembre?", "30",
+            false, account);
     }
 
     virtual void TearDown(){
         delete settedSecurityManager;
+        delete account;
     }
 
     SecurityManager* settedSecurityManager;
+    Account* account;
 };
 
 TEST_F(SecurityManagerSuite, checkAnswerTest) {
@@ -25,6 +30,13 @@ TEST_F(SecurityManagerSuite, checkAnswerTest) {
     EXPECT_FALSE(settedSecurityManager->checkAnswer("31"));
 
     EXPECT_TRUE(settedSecurityManager->checkAnswer("30"));
+}
+
+TEST_F(SecurityManagerSuite, checkCurrentPINTest) {
+
+    EXPECT_EQ(account->getPIN(),"12345");
+
+    EXPECT_TRUE(settedSecurityManager->checkCurrentPIN("12345"));
 }
 
 TEST_F(SecurityManagerSuite, changeDigitalKeyTest) {
@@ -43,4 +55,16 @@ TEST_F(SecurityManagerSuite, changeSecurityQuestionTest) {
     //assert
     ASSERT_EQ(settedSecurityManager->getQuestion(), "Quanti giorni ha marzo?");
     ASSERT_TRUE(settedSecurityManager->checkAnswer("31"));
+}
+
+TEST_F(SecurityManagerSuite, setNewPinTest) {
+    //verify preconditions
+    EXPECT_TRUE(settedSecurityManager->checkCurrentPIN("12345"));
+
+    settedSecurityManager->setNewPin("55555");
+
+    ASSERT_FALSE(settedSecurityManager->checkCurrentPIN("12345"));
+    ASSERT_TRUE(settedSecurityManager->checkCurrentPIN("55555"));
+
+    ASSERT_EQ(account->getPIN(),"55555");
 }
